7d8b9910f9c94ff2cd9e076a2f505f2f2dd4fce1
Joe Lockhart <joe.lockhart0@gmail.com>
2023-10-02 14:23:24 +0100

    4 nearly there

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const {getTopics} = require('./controllers/topics-controllers.js')
+const {getArticleById} = require('./controllers/articles.controllers.js')
 const { handleMispelledPath } = require('./controllers/errors.controllers.js')
 
 app = express()
 
 app.get('/api/topics', getTopics)
 
+app.get('/api/articles/:article_id', getArticleById)
+
 app.get('/*', handleMispelledPath)
 
 
 module.exports = { app }
+
