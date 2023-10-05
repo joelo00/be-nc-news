6fbb77e2b6fb2feab35d9f35e7f382ae59e667e2
Joe Lockhart <joe.lockhart0@gmail.com>
2023-10-05 12:09:39 +0100

    finally done

--- routes/articles-router.js
+++ routes/articles-router.js
@@ -1,4 +1,4 @@
-const { getArticleById, getArticles, getCommentsOnArticle, patchArticleById, postCommentOnArticle, postArticle } = require('../controllers/articles.controllers.js')
+const { getArticleById, getArticles, getCommentsOnArticle, patchArticleById, postCommentOnArticle, postArticle, deleteArticleById } = require('../controllers/articles.controllers.js')
 
 const articleRouter = require('express').Router();
 
@@ -10,6 +10,7 @@ articleRouter.get('/:article_id/comments', getCommentsOnArticle)
 articleRouter.patch('/:article_id', patchArticleById)
 articleRouter.post('/:article_id/comments', postCommentOnArticle)
 articleRouter.post('/', postArticle)
+articleRouter.delete('/:article_id', deleteArticleById)
 
 
 module.exports = articleRouter
